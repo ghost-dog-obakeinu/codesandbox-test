b0f3bdee49e418419b3b506cb129fc239a8d8898
おばけ犬 <101488927+ghost-dog-obakeinu@users.noreply.github.com>
2023-01-08 22:40:14 +0900

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import "./styles.css";
+/**
+ * const, letなどの変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-console.log("test");
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+// console.log(val2);
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const変数は上書き不可能
+// val3 = "const変数を上書き";
+// console.log(val3);
+
+// const val4 = {
+//   name: "ghost-dog",
+//   age: 24
+// };
+// // constオブジェクトの中身は上書きできてしまう
+// val4.age = 25;
+// // 属性の追加も可能
+// val4.address = "Tokyo";
+// console.log(val4);
+
+// const配列の中身は上書きできてしまう
+// なんか行選択しなくてもカーソル合わせるだけで行ごとコピーできる
+// const val5 = ["dog1", "dog2", "dog3"];
+// val5[0] = "dog0";
+// val5.push("dog4");
+// console.log(val5);
